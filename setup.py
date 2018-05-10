43fc33db75c7a70faab1c73eda16884567fe0141
Torres <monteirotorres@gmail.com>
2018-05-10 17:22:04 +0100

    Tweaking for Pypi

--- setup.py
+++ setup.py
@@ -1,4 +1,14 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
   name = 'Structuralia',
   packages = ['Structuralia'], 
