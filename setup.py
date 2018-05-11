64031a2fe3d44ac381e53c37e57b82e4d6e64499
Torres <monteirotorres@gmail.com>
2018-05-11 14:28:03 +0100

    Resolving wheel issues

--- setup.py
+++ setup.py
@@ -12,13 +12,13 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
   name = 'Structuralia',
   packages = find_packages(),
-  version = '0.903',
+  version = '0.904',
   description = 'A toolset to work with proteins',
   long_description = long_description,
   author = 'Pedro Torres',
   author_email = 'monteirotorres@gmail.com',
   url = 'https://github.com/monteirotorres/Structuralia',
-  download_url = 'https://github.com/monteirotorres/Structuralia/archive/0.903.tar.gz',
+  download_url = 'https://github.com/monteirotorres/Structuralia/archive/0.904.tar.gz',
   keywords = ['PDB', 'structure', 'protein'],
   classifiers = [],
   py_modules=[
